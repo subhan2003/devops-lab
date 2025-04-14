c909293eff472aaf4a90ac3aadbf5c30f866d5c8
Akbar Ali Shah <akbaralishah@Akbars-MacBook-Air.local>
2025-04-14 00:46:14 -0400

    new version

--- README.md
+++
@@ -1,2 +0,0 @@
-# devops-lab
-A hands-on lab project to learn and experiment with CI/CD workflows using GitHub Actions. Includes automated testing, linting, and basic deployment for Python-based applications.
--- app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-# Create a Blueprint for the main app routes
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    return render_template('index.html')
